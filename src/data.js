e9c29879a3ed7e39e6031a46cd6c6cf7adbd4245
Apoorva0601 <apoorvaspoojary0601@gmail.com>
2025-08-30 09:42:40 +0000

    edited

--- src/data.js
+++ src/data.js
@@ -62,7 +62,7 @@ export const projects = [
     id: 1,
     title: "Real-Time Chat App",
     description: "A sleek MERN Chat App with a clean, responsive UI and real-time messaging powered by Socket.io. It allows users to create accounts, join chat rooms, and exchange messages instantly. Built using React and Node.js, it features JWT authentication and uses MongoDB for efficient data storage and retrieval.",
-    image: "/project1.jpg", // Replace with actual project image
+    image: "/project1.png", // Replace with actual project image
     techStack: ["React", "Node.js", "MongoDB", "Express", "Tailwind CSS", "Socket.io"],
     liveDemo: "https://chat-app-psi-pink.vercel.app/login",
     github: "https://github.com/Anvithshetty17/chat-app",
@@ -72,7 +72,7 @@ export const projects = [
     id: 2,
     title: "Fitness Tracker",
     description: "A sleek MERN Fitness Tracker app with a clean, responsive UI and modern design. It allows users to log daily workouts like running, yoga, or gym, along with duration and date. Built using React and Node.js, it also integrates Axios and MongoDB for efficient data handling and real-time updates.",
-    image: "/project2.jpg", // Replace with actual project image
+    image: "/project2.png", // Replace with actual project image
     techStack: ["React",  "Node.js", "MongoDB", "Express"],
     liveDemo: "https://fitness-tracker-olive-rho.vercel.app/",
     github: "https://github.com/Apoorva0601/fitness_tracker.git",
@@ -82,7 +82,7 @@ export const projects = [
     id: 3,
     title: "Digital Student ID Card",
     description: "A sleek Flutter Student ID Card app with a responsive UI and modern gradient design. It features an animated profile image, institutional branding, and neatly displays key student details such as name, course, and USN. Additionally, it generates a barcode for the USN to simulate real-world ID scanning functionality.",
-    image: "/project3.jpg", // Replace with actual project image
+    image: "/project3.png", // Replace with actual project image
     techStack: ["Flutter","Dart"],
     liveDemo: "https://github.com/Apoorva0601/ID_CARD.git",
     github: "https://github.com/Apoorva0601/ID_CARD.git",
@@ -92,7 +92,7 @@ export const projects = [
     id: 4,
     title: "Women's Safety Analyzer",
     description: "This Python project analyzes Instagram comments on women's safety in India using Natural Language Processing (NLP). It features a Tkinter-based GUI that allows users to load CSV files, clean the data, and perform sentiment analysis using the TextBlob library. Results are shown using Matplotlib charts for clear public opinion insights.",
-    image: "/project4.jpg", // Replace with actual project image
+    image: "/project4.png", // Replace with actual project image
     techStack: [],
     liveDemo: "https://github.com/Apoorva0601/women_safety",
     github: "https://github.com/Apoorva0601/women_safety",
@@ -102,7 +102,7 @@ export const projects = [
     id: 5,
     title: "Interactive Quiz App",
     description: "The Quiz App is a responsive web application built using HTML, CSS, and JavaScript. It offers a smooth and engaging interface for users to test their knowledge through multiple-choice questions. The app features dynamic question rendering, real-time score tracking, and instant feedback, making it ideal for learning and self-assessment",
-    image: "/project5.jpg", // Replace with actual project image
+    image: "/project5.png", // Replace with actual project image
     techStack: ["HTML", "CSS", "JavaScript"],
     liveDemo: "https://apoorvapoojary-quiz-app.vercel.app/",
     github: "https://github.com/Apoorva0601/Quiz_app",
@@ -112,7 +112,7 @@ export const projects = [
     id: 6,
     title: "Weather App",
     description: "A simple and interactive weather application built using HTML, CSS, and JavaScript. It fetches real-time weather data based on user input and displays temperature, conditions, and location info with a clean and responsive UI. Perfect for learning API integration and frontend development basics.",
-    image: "/project6.jpg", // Replace with actual project image
+    image: "/project6.png", // Replace with actual project image
     techStack: ["HTML", "CSS", "JavaScript","Weather API"],
     liveDemo: "https://apoorvapoojary-weather-app.vercel.app/",
     github: "https://github.com/Apoorva0601/Weather_app",
@@ -128,7 +128,7 @@ export const courses = [
     duration: "40 hours",
     completedDate: "2023",
     certificateLink: "https://certificate-link.com",
-    image: "/course1.jpg" // Replace with actual course image
+    image: "/course1.png" // Replace with actual course image
   },
   {
     id: 2,
@@ -137,7 +137,7 @@ export const courses = [
     duration: "35 hours",
     completedDate: "2023",
     certificateLink: "https://certificate-link.com",
-    image: "/course2.jpg" // Replace with actual course image
+    image: "/course2.png" // Replace with actual course image
   },
   {
     id: 3,
@@ -146,7 +146,7 @@ export const courses = [
     duration: "28 hours",
     completedDate: "2022",
     certificateLink: "https://certificate-link.com",
-    image: "/course3.jpg" // Replace with actual course image
+    image: "/course3.png" // Replace with actual course image
   },
   {
     id: 4,
@@ -155,7 +155,7 @@ export const courses = [
     duration: "300 hours",
     completedDate: "2022",
     certificateLink: "https://certificate-link.com",
-    image: "/course4.jpg" // Replace with actual course image
+    image: "/course6.png" // Replace with actual course image
   },
   {
     id: 5,
@@ -164,7 +164,16 @@ export const courses = [
     duration: "20 hours",
     completedDate: "2023",
     certificateLink: "https://certificate-link.com",
-    image: "/course5.jpg" // Replace with actual course image
+    image: "/course5.png" // Replace with actual course image
+  },
+  {
+    id: 6,
+    name: "MongoDB Developer Path",
+    platform: "MongoDB University",
+    duration: "20 hours",
+    completedDate: "2023",
+    certificateLink: "https://certificate-link.com",
+    image: "/course4.png" // Replace with actual course image
   }
 ];
 
